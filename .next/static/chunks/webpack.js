4a1082d625229743238d325e6fe24658d8e21d56
claireyyu <claireyueyuca@gmail.com>
2024-10-02 13:37:06 -0700

    Change border radius

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "790a200f73082415"; }
+/******/ 		__webpack_require__.h = function() { return "470619d2b11a46ee"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
