4e607f91f9cf546010c6f5bedfe6f4d89277629f
claireyyu <claireyueyuca@gmail.com>
2024-10-11 10:53:32 -0700

    Change animation for page transition

--- src/app/template.js
+++ src/app/template.js
@@ -5,9 +5,9 @@ import { motion } from "framer-motion"
 export default function Template({children}) {
     return (
       <motion.div
-        initial={{ y: 15, opacity: 0.75 }}
+        initial={{ y: 20, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
-        transition={{ ease: "linear", duration: 0.75 }}
+        transition={{ ease: "easeInOut", duration: 0.75 }}
       >
         {children}
       </motion.div>
