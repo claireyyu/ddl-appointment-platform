c4c4a828c678bf7e8268e99e563d2bf5730c5eac
claireyyu <claireyueyuca@gmail.com>
2024-11-06 09:56:26 -0800

    Add name input field to SignupPage form

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -6,6 +6,13 @@ export default function SignupPage() {
       <form className="flex flex-col space-y-4 my-48 p-40 bg-foreground text-background rounded-custom">
         <div className="flex space-x-4">
           <label className="flex-1">
+            Name:
+          </label>
+          <input type="text" name="name" className="border-background border-2 rounded-custom"/>
+        </div>
+
+        <div className="flex space-x-4">
+          <label className="flex-1">
             Email:
           </label>
           <input type="text" name="email" className="border-background border-2 rounded-custom"/>
