05072853f24650ac517a6bbb5e411d0460d439ee
claireyyu <claireyueyuca@gmail.com>
2024-11-25 11:07:50 -0800

    Refactor ResultPage layout; adjust padding in ResultButtons for improved responsiveness.

--- src/components/ResultButtons/ResultButtons.tsx
+++ src/components/ResultButtons/ResultButtons.tsx
@@ -12,13 +12,13 @@ const ResultButtons: React.FC<ResultButtonsProps> = ({ activeTab, setActiveTab }
   return (
     <div className="flex flex-col space-y-4 mt-8">
       <button
-        className={`px-8 xl:px-12 py-2 flex justify-center rounded-custom shadow-button ${activeTab === 'bazi' ? 'bg-gradient-to-r from-bStart to-bEnd text-foreground' : 'bg-foreground text-black'}`}
+        className={`px-2 xl:px-12 py-2 flex justify-center rounded-custom shadow-button ${activeTab === 'bazi' ? 'bg-gradient-to-r from-bStart to-bEnd text-foreground' : 'bg-foreground text-black'}`}
         onClick={() => setActiveTab('bazi')}
       >
         {t('bazi')}
       </button>
       <button
-        className={`px-8 xl:px-12 py-2 flex justify-center rounded-custom shadow-button ${activeTab === 'liupan' ? 'bg-gradient-to-r from-bStart to-bEnd text-foreground' : 'bg-foreground text-black'}`}
+        className={`px-2 xl:px-12 py-2 flex justify-center rounded-custom shadow-button ${activeTab === 'liupan' ? 'bg-gradient-to-r from-bStart to-bEnd text-foreground' : 'bg-foreground text-black'}`}
         onClick={() => setActiveTab('liupan')}
       >
         {t('liupan')}
