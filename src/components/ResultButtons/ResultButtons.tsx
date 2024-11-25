1e80d800681c715c0ce72ff1fcfb62ed6318fe0d
claireyyu <claireyueyuca@gmail.com>
2024-11-25 11:54:45 -0800

    Refactor ResultButtons and ResultPaipan components; adjust layout for improved responsiveness and localization support.

--- src/components/ResultButtons/ResultButtons.tsx
+++ src/components/ResultButtons/ResultButtons.tsx
@@ -10,7 +10,7 @@ const ResultButtons: React.FC<ResultButtonsProps> = ({ activeTab, setActiveTab }
   const t = useTranslations('ResultButtons');
 
   return (
-    <div className="flex justify-center items-center xl:flex-col space-x-4 xl:space-x-0 xl:space-y-4 mt-8">
+    <div className="flex justify-center xl:flex-col space-x-4 xl:space-x-0 xl:space-y-4">
       <button
         className={`px-2 xl:px-12 py-2 flex justify-center rounded-custom shadow-button ${activeTab === 'bazi' ? 'bg-gradient-to-r from-bStart to-bEnd text-foreground' : 'bg-foreground text-black'}`}
         onClick={() => setActiveTab('bazi')}
