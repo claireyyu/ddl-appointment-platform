5a05f44098b1b3d8a33a18f254dbf2b73398ce05
claireyyu <claireyueyuca@gmail.com>
2024-12-13 21:35:32 -0800

    Add RootLayout component and update package dependencies for localization and image processing

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -13,7 +13,7 @@ export const routing = defineRouting({
   // }
 });
 
-export type Pathnames = keyof typeof routing.pathnames;
+// export type Pathnames = keyof typeof routing.pathnames;
 export type Locale = (typeof routing.locales)[number];
 
 export const {Link, getPathname, redirect, usePathname, useRouter} =
